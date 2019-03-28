d62e3da6115ea57935246b630dfbc44ccdd9eeb7
geert <geert_depuydt@hotmail.com>
2019-03-28 10:25:46 +0100

    allocator implementation

--- fnd/src/alloc/win32_heap.rs
+++ fnd/src/alloc/win32_heap.rs
@@ -4,7 +4,7 @@ use core::ffi::c_void;
 //https://doc.rust-lang.org/core/ptr/struct.NonNull.html
 use core::ptr::NonNull;
 use super::Layout;
-//use super::Allocator;
+use super::Allocator;
 
 
 type HANDLE = *mut c_void;
@@ -24,5 +24,38 @@ extern "system" {
     // For more details about these win32 functions can be found,
     // here: https://docs.microsoft.com/en-us/windows/desktop/api/heapapi/
     fn GetProcessHeap() -> HANDLE;
-    // TODO Geert: add HeapAlloc and HeapFree functions
+    fn HeapAlloc(heap: HANDLE, flags: u32, byte: usize) -> *mut c_void; // should be equivalent to HANDLE, no?
+    fn HeapFree(heap: HANDLE, flags: u32, mem: *mut c_void) -> BOOL;
+}
+///implement Default trait for giving a type a useful default value
+impl Default for Win32HeapAllocator {
+    fn default() -> Self {
+        Self {
+            heap: unsafe {GetProcessHeap()},
+        }
+    }
+}
+
+impl Allocator for &Win32HeapAllocator {
+    unsafe fn alloc (&mut self, layout: Layout) -> Option<NonNull<c_void>> {
+        let ptr = HeapAlloc(self.heap, 0, layout.size);
+        if ptr.is_null() {
+            None
+        }
+        else {
+            // Creates a new NonNull pointer type.
+            // More information can be found at 
+            // https://doc.rust-lang.org/std/ptr/struct.NonNull.html#method.new_unchecked
+            Some(NonNull::new_unchecked(ptr))
+        }
+
+    }
+
+    unsafe fn dealloc (&mut self, ptr: *mut c_void) {
+        HeapFree(self.heap, 0, ptr);
+    }
+
+
+
+
 }
